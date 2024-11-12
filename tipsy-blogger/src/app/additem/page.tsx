31d8238ad595f5a810c6c39a97d71cad04deb615
Cooperw17 <weishaarcooper@gmail.com>
2024-11-11 19:23:05 -0500

    form page w/css
    updated the form page, created img folder, and added css to form page as well as header.

--- tipsy-blogger/src/app/additem/page.tsx
+++ tipsy-blogger/src/app/additem/page.tsx
@@ -1,11 +1,49 @@
-import Header from "../components/Header"
+import React from 'react';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Link from 'next/link';
+import './addpage.css';
 
-export default function() {
-    return(
-        <div>
-        <Header />
-        <Header />
-        <Header />
+const Page: React.FC = () => {
+  return (
+    <>
+      <Header>
+        <Button>
+          <Link href="/authview">Login</Link>
+        </Button>
+        <Button>
+          <Link href="/additem">Sign Up</Link>
+        </Button>
+      </Header>
+
+      <div className="form-background">
+        <div className="background-wrapper">
+          
         </div>
-    );
-}
+        <div className="form-container">
+          <h1 className="form-title">THE TIPSY BLOGGER</h1>
+          <form className="form-fields">
+            <label htmlFor="location">Location:</label>
+            <input type="text" id="location" name="location" />
+
+            <label htmlFor="barRating">Bar Rating:</label>
+            <input type="number" id="barRating" name="barRating" />
+
+            <label htmlFor="drinkOfChoice">Drink of Choice:</label>
+            <input type="text" id="drinkOfChoice" name="drinkOfChoice" />
+
+            <label htmlFor="drinkRating">Drink Rating:</label>
+            <input type="number" id="drinkRating" name="drinkRating" />
+
+            <label htmlFor="review">Review:</label>
+            <textarea id="review" name="review" />
+
+            <button type="submit" className="submit-button">Submit</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
